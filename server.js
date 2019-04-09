d5941a145cf61295371582de43164f300359e882
donald-stolz <donaldtstolz@gmail.com>
2019-04-09 12:18:25 -0700

    Add loop to sync all data

--- server.js
+++ server.js
@@ -42,44 +42,49 @@ app.unsubscribe(bodyParser.urlencoded({ extended: false }));
 
 // TODO: Need a one time setup to sync our DB with intra login/image_url data
 const syncIntraFirebase = async () => {
-  let studentsH2S = await intraRequest(
-    "https://api.intra.42.fr/v2/cursus/17/cursus_users?filter%5Bactive%5D=true&filter%5Bcampus_id%5D=7&page%5Bsize%5D=100&per_page",
-    (err, response, body) => {
-      if (!err) {
-        let studentsH2S = JSON.parse(body);
-        // NOTE: Is this all H2S students
-        return studentsH2S;
-      } else {
-        console.error(err);
-      }
-    }
-  );
+  let i = 1;
 
-  studentsH2S.forEach(async item => {
-    try {
-      const { login } = item.user;
-      let details = await intraLimited(
-        `https://api.intra.42.fr/v2/users/${login}`,
-        (err, res) => {
-          if (err) {
-            console.error(err);
-          } else if (res.statusCode === 429) {
-            console.log("Rate limit excedded");
-          }
+  while (i < 14) {
+    let studentsH2S = await intraRequest(
+      `https://api.intra.42.fr/v2/cursus/17/cursus_users?filter%5Bactive%5D=true&filter%5Bcampus_id%5D=7&page=${i}`,
+      (err, response, body) => {
+        if (!err) {
+          let studentsH2S = JSON.parse(body);
+          // NOTE: Is this all H2S students
+          return studentsH2S;
+        } else {
+          console.error(err);
         }
-      );
+      }
+    );
 
-      let { displayname, image_url } = details;
-      let student = { login, displayname, image_url };
-      console.log(student);
+    studentsH2S.forEach(async (item, index) => {
+      try {
+        const { login } = item.user;
+        let details = await intraLimited(
+          `https://api.intra.42.fr/v2/users/${login}`,
+          (err, res) => {
+            if (err) {
+              console.error(err);
+            } else if (res.statusCode === 429) {
+              console.log("Rate limit excedded");
+            }
+          }
+        );
 
-      db.ref("students").push(student);
-    } catch (error) {
-      console.error(error);
-    }
-  });
+        let { displayname, image_url } = details;
+        let student = { login, displayname, image_url };
+        console.log(index, student);
+        db.ref("students").push(student);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+    i++;
+  }
 };
 
+syncIntraFirebase();
 /****************/
 /* 				*/
 /* Begin Routes */
