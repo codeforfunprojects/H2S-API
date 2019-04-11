325810d347c74544765dd6eced41ca73e7713f31
Donald Stolz <donaldtstolz@gmail.com>
2019-04-10 20:11:12 -0700

    Update for hosting

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
